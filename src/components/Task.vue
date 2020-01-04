de2e74503c50a45991fea659f05c5e3ff0716620
k.sato <keisuke.sato.gp@gmail.com>
2020-01-04 14:39:46 +0900

    modify timer processing

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -3,7 +3,7 @@
     <p class="task__name">{{ taskName }}</p>
     <div class="timer">
       <div class="timer__time">
-        <p class="timer__time-total">{{ timeTotal }}</p>
+        <!-- <p class="timer__time-total">{{ timeTotal }}</p> -->
         <p class="timer__time-state">{{ timeState }}</p>
       </div>
       <div class="timer__button">
@@ -16,15 +16,13 @@
 </template>
 
 <script>
-//let startTime;
-
 export default {
   name: 'task',
   data(){
     return {
       taskName: 'タスク名',
       isMeasuring: false,
-      timeTotal: '00:00:00',
+      timeTotal: 0,
       timeState: '00:00:00',
       startTime: 0,
       //time: ''
@@ -41,10 +39,11 @@ export default {
     countStop: function(){
       this.isMeasuring = false
       clearTimeout(this.timerId);
+      this.timeTotal += Date.now() - this.startTime;
     },
     countTime: function(){
       
-      const d = new Date(Date.now() - this.startTime);
+      const d = new Date(Date.now() - this.startTime + this.timeTotal);
       const h = String(d.getUTCHours()).padStart(2, '0');
       const m = String(d.getMinutes()).padStart(2, '0');
       const s = String(d.getSeconds()).padStart(2, '0');
