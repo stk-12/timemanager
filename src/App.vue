db05ad2ac5c6c4a361f8ce583df7be66b7937657
k.sato <keisuke.sato.gp@gmail.com>
2020-01-04 13:13:26 +0900

    add timer processing

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Task />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Task from './components/Task.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Task
   }
 }
 </script>
---
+++ src/components/Task.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="task">
+    <p class="task__name">{{ taskName }}</p>
+    <div class="timer">
+      <div class="timer__time">
+        <p class="timer__time-total">{{ timeTotal }}</p>
+        <p class="timer__time-state">{{ timeState }}</p>
+      </div>
+      <div class="timer__button">
+        <button class="timer__button-start" v-on:click="countStart" v-if="!isMeasuring">start</button>
+        <button class="timer__button-stop" v-on:click="countStop" v-if="isMeasuring">stop</button>
+      </div>
+    </div>
+    <pre>{{ $data }}</pre>
+  </div>
+</template>
+
+<script>
+//let startTime;
+
+export default {
+  name: 'task',
+  data(){
+    return {
+      taskName: 'タスク名',
+      isMeasuring: false,
+      timeTotal: '00:00:00',
+      timeState: '00:00:00',
+      startTime: 0,
+      //time: ''
+      timerId: ''
+    }
+  },
+  methods: {
+    countStart: function(){
+      this.startTime = Date.now();
+      this.isMeasuring = true
+      this.countTime();
+      
+    },
+    countStop: function(){
+      this.isMeasuring = false
+      clearTimeout(this.timerId);
+    },
+    countTime: function(){
+      
+      const d = new Date(Date.now() - this.startTime);
+      const h = String(d.getUTCHours()).padStart(2, '0');
+      const m = String(d.getMinutes()).padStart(2, '0');
+      const s = String(d.getSeconds()).padStart(2, '0');
+
+      this.timeState = `${h}:${m}:${s}`;
+
+      this.timerId = setTimeout(() => {
+        this.countTime();
+      }, 10);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
